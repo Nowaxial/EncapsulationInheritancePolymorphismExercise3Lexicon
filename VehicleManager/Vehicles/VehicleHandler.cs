76b9cfa3ed1ad04096bc192f6fd398164615414f
Nowaxial <Nowaxial@users.noreply.github.com>
2025-04-08 22:54:26 +0200

    Add VehicleHandler class for managing vehicles
    Introduces the VehicleHandler class in the VehicleManager.Vehicles namespace.
    This class includes a private list of Vehicle objects and a method to add
    new vehicles to the list, enhancing vehicle management functionality.

---
+++ VehicleManager/Vehicles/VehicleHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleManager.Vehicles;
+
+public class VehicleHandler
+{
+    private List<Vehicle> vehicles = new List<Vehicle>();
+    public void AddVehicle(Vehicle vehicle) 
+    {  
+        vehicles.Add(vehicle); 
+    }
+}
